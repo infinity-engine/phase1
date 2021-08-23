a92018020c5955b6c41f263638820030e71b9b2b
Sam <koushik.engine@gmail.com>
2021-08-23 20:45:46 +0530

    Initial Setup

---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import math
+
+#Initiate the Optimization Method Here
+def start():
+    #take input from the user and run the methods
+    pass
+
+# Bracketing Method
+def ExhaustiveSearchMethod(a,b):
+    pass
+
+# Bounding Phase method
+def intervalHalving(a,b):
+    pass
+
+# Objective functions' definition
+def objectiveFunction1(x):
+	pass
+
+def objectiveFunction2(x):
+	pass
+
+def objectiveFunction3(x):
+	pass
+
+def objectiveFunction4(x):
+	pass
+
+def objectiveFunction5(x):
+	pass
+
+def objectiveFunction6(x):
+	pass
+
+
+
+# Run the code from here.
+start()
