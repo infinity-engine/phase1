6562b36dc193f7dbd9bf21b26f909065f251b920
Sam <koushik.engine@gmail.com>
2021-09-03 20:11:03 +0530

    Plot add

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+from typing import List
 import matplotlib.pyplot as plt
 import math
 import random
 
 # Global Variables and Data Structures
+objectiveFunctionIndicator = 1
+epsinol = 10**-3
+delta = 0.0198
+lowerLimit = -10
+upperLimit = 0.5
+noOf_functionEval = 0
+queueSize = 5
+myQ_1 = None  # Queue
+out = open(r"Phase_1_iterations.out", "w")
 
-# Let make a Queue(data structure) which will store the recent x values and corresponding functionvalues
-# we will only store 3 values at this point
+#Bounding Phase Method varibales for ploting
+x_series = []
+f_x_series = []
+x_1_series = []
+f_x_1_series = []
+x_2_series =[]
+f_x_2_series = []
 
+#Interval Halving Method Variables for ploting
+x_m_series = []
+f_x_m_series = []
 
+# Let make a Queue(data structure) which will store the recent x values and corresponding function values
+# we will only store limited values at this point
 class CustomQueue:
+    global queueSize
     def __init__(self) -> None:
         self.x_values = []
         self.function_values = []
@@ -19,46 +40,63 @@ class CustomQueue:
         self.function_values.append(f)
 
         # now pope the first item from both the que
-        if(len(self.x_values) > 3):
+        if(len(self.x_values) > queueSize):
             self.x_values.pop(0)
             self.function_values.pop(0)
         else:
             pass
 
 
-objectiveFunctionIndicator = 1
-epsinol = 10**-3
-delta = 0.0198
-lowerLimit = -10
-upperLimit = 0.5
-noOf_functionEval = 0
-myQ_1 = CustomQueue()  # Queue
-
 # Initiate the Optimization Method Here
 def start():
     # take input from the user and run the methods
-
+    global out,noOf_functionEval,myQ_1
 
     #set no of function evaluation = 0
-    global noOf_functionEval
     noOf_functionEval = 0
-
-    global myQ_1
+    
     #reinitialise the queue
     myQ_1 = CustomQueue()
     [a, b] = boundingPhaseMethod(lowerLimit, upperLimit)
+    
     print(f"Bounding Phase Method -> {a} to {b}")
     print(f"No of function evaluations at the end of Bracketing Method = {noOf_functionEval}")
+    
+    plt.figure(1)
+    plt.title("Bounding Phase Method")
+    plt.xlabel("x in various iterations")
+    plt.ylabel("F(x) in various Iterations")
+    plt.plot(x_series,f_x_series,"r*-")
+    
 
-    #reinitialise the queue
+    #re-initialise the queue
     myQ_1 = CustomQueue()
+    
+    out.write("\n\n")
+    
+    #call the region elimination method
     [a, b] = intervalHalving(a, b)
+    
     print(f"Interval Halving Method -> {a} to {b}")
     print(f"Total no of function evaluations = {noOf_functionEval}")
+    
+    plt.figure(2)
+    plt.title("Interval Halving Method")
+    plt.xlabel("x_m in various iterations")
+    plt.ylabel("F(x_m) in various iterations")
+    plt.plot(x_m_series,f_x_m_series,"b+-")
+    
+    plt.show()
 
 
 # Bracketing Method
 def boundingPhaseMethod(a, b):
+    global out,x_series,f_x_series
+    
+    #Write in the output File
+    out.write("Bounding Phase Method - Results\n")
+    out.write("#Iteration\t\t\tx\t\t\t\t\tf(x)\n")
+    
     k = 0
     deltaWithSign = 0
     while True:
@@ -74,6 +112,13 @@ def boundingPhaseMethod(a, b):
             deltaWithSign = - abs(delta)
         else:
             continue
+        
+        #Write the initial value of x and f(x)
+        out.write("{}\t\t\t{}\t\t{}\n".format(k,x_0,objectiveFunction(x_0)))
+        
+        #Fill the data in the List for ploting
+        x_series.append(x_0)
+        f_x_series.append(objectiveFunction(x_0))
 
         while True:
             # step 3
@@ -82,6 +127,12 @@ def boundingPhaseMethod(a, b):
             if objectiveFunction(x_new) < objectiveFunction(x_0):
                 k += 1
                 x_0 = x_new
+                #write the new value of x and f(x)
+                out.write("{}\t\t\t{}\t\t{}\n".format(k,x_new,objectiveFunction(x_new)))
+                
+                #Fill the data in the List for ploting
+                x_series.append(x_new)
+                f_x_series.append(objectiveFunction(x_new))
                 continue
             else:
                 return [x_new-(2**k)*1.5*deltaWithSign, x_new]
@@ -89,10 +140,23 @@ def boundingPhaseMethod(a, b):
 
 # Bounding Phase method
 def intervalHalving(a, b):
+    global x_m_series,f_x_m_series
+    
     # step 1
     x_m = (a+b)/2
     l = b-a
     no_of_iteration = 1
+    
+    #Write in the output File
+    out.write("Interval Halving Method - Results\n")
+    out.write("#Iteration\t\t\tx_1\t\t\t\t\tx_m\t\t\tx_2\t\t\t\t\tf(x_1)\t\t\t\t\tf(x_m)\t\t\t\t\tf(x_2)\n")
+    
+    #Write the initial value of x and f(x)
+    out.write("{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t{}\n".format(no_of_iteration,a,x_m,b,objectiveFunction(a),objectiveFunction(x_m),objectiveFunction(b)))
+    
+    #Fill the data in the store as well
+    x_m_series.append(x_m)
+    f_x_m_series.append(objectiveFunction(x_m))
 
     while True:
         # step2
@@ -111,17 +175,26 @@ def intervalHalving(a, b):
                 a = x_m
                 x_m = x_2
                 break
+            
             else:
                 a = x_1
                 b = x_2
                 break
 
         # step5
+        #Write the value of x and f(x)
+        out.write("{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t{}\n".format(no_of_iteration,a,x_m,b,objectiveFunction(a),objectiveFunction(x_m),objectiveFunction(b)))
+    
+        #Fill the data in the store as well
+        x_m_series.append(x_m)
+        f_x_m_series.append(objectiveFunction(x_m))
+        
         l = b-a
         if l < epsinol:
             return [a, b]
             break
         else:
+            no_of_iteration += 1
             continue
 
 
